dc87c793a3ee6422efe90f8bb8a55076ea94203c
unknown <vpupkin@company.com>
2020-10-01 16:35:17 +0300

    Add 8 excercise

---
+++ scripts/excercise-8.js
@@ -0,0 +1,41 @@
+const refs = {
+  boxesContainer: document.getElementById('boxes'),
+  input: document.querySelector('input[type="number"]'),
+  buttonRender: document.querySelector('[data-action="render"]'),
+  buttonDestroy: document.querySelector('[data-action="destroy"]'),
+};
+
+refs.buttonRender.addEventListener('click', onButtonRenderClick);
+refs.buttonDestroy.addEventListener('click', onButtonDestroyClick);
+
+function onButtonRenderClick() {
+  onButtonDestroyClick();
+  createBoxes();
+}
+
+function createBoxes(amount) {
+  amount = document.querySelector('input[type="number"]').valueAsNumber;
+  if (amount <= 0) {
+    return;
+  }
+  const boxSize = 30;
+  let murkupOfBoxes = '';
+  for (let i = 0; i < amount; i += 1) {
+    murkupOfBoxes += `<div class='box' style="width: ${
+      boxSize + 10 * i
+    }px; height = ${boxSize + 10 * i}px; background-color: rgb(${getRandomColor(
+      255,
+    )}, ${getRandomColor(255)}, ${getRandomColor(255)});"></div>`;
+  }
+
+  refs.boxesContainer.insertAdjacentHTML('beforeend', murkupOfBoxes);
+  console.log(murkupOfBoxes);
+
+  function getRandomColor(value) {
+    return Math.floor(Math.random() * Math.floor(value));
+  }
+}
+
+function onButtonDestroyClick() {
+  document.querySelectorAll('box').remove();
+}
